a348d65390f33aad5f9b1a3a3bdfbbea086df8e2
Celsuss <jenslorden@proton.me>
2025-06-25 00:50:22 +0200

    Adding a argo-cd root app

--- helm/scripts/deploy-argo-cd.sh
+++ helm/scripts/deploy-argo-cd.sh
@@ -6,7 +6,6 @@ helm repo add argo-helm https://argoproj.github.io/argo-helm
 helm repo update
 helm dep update charts/argo-cd/
 
-
 # Create namespace
 kubectl create namespace argo-cd --dry-run=client -o=json | kubectl apply -f -
 
@@ -17,6 +16,6 @@ helm upgrade --install argo-cd ../charts/argo-cd \
 
 echo "Argo-cd deployment complete!"
 echo "Run 'kubectl port-forward svc/argo-cd-argocd-server 8080:443 -n argo-cd' to enable access to UI."
-echo "Go to http://localhost:8080"
 echo "Username: Admin"
 echo "Get password by running 'kubectl get secret argocd-initial-admin-secret -o jsonpath=\"{.data.password}\" -n argo-cd | base64 -d'"
+echo "Go to http://localhost:8080"
