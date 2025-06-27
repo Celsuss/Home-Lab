b7379b19e26600cabce8b56aed4ffda19c44b60d
Celsuss <jenslorden@proton.me>
2025-06-27 22:42:08 +0200

    Updated argo-cd to work on K3s

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Home-Lab
 Home lab setup
+
+## Deployment
+1. Make sure you have a connection to a kubernetes cluster.
+2. Go to `/scripts/` and run `/deploy-argo-cd.sh`.
--- helm/scripts/deploy-argo-cd.sh
+++ helm/scripts/deploy-argo-cd.sh
@@ -4,18 +4,27 @@ set -e
 # Add repo
 helm repo add argo-helm https://argoproj.github.io/argo-helm
 helm repo update
-helm dep update charts/argo-cd/
+helm dep update ../charts/argo-cd/
 
 # Create namespace
 kubectl create namespace argo-cd --dry-run=client -o=json | kubectl apply -f -
 
+# Manage certificate for k3s
+sudo cat /var/lib/rancher/k3s/server/tls/server-ca.crt > k3s-ca.crt
+kubectl create secret generic k3s-ca-cert \
+        --from-file=ca.crt=k3s-ca.crt \
+        -n argo-cd \
+        --dry-run=client \
+        -o yaml | kubectl apply -f -
+
+# Deploy chart
 helm dependency build ../charts/argo-cd
 helm upgrade --install argo-cd ../charts/argo-cd \
      -f ../charts/argo-cd/values.yaml \
      --namespace argo-cd
 
 echo "Argo-cd deployment complete!"
-echo "Run 'kubectl port-forward svc/argo-cd-argocd-server 8080:443 -n argo-cd' to enable access to UI."
+echo "Get Node ip from running 'kubectl get nodes -o wide'"
+echo "Add 'NODE-IP argocd.homelab.local' to '/etc/hosts'."
 echo "Username: Admin"
 echo "Get password by running 'kubectl get secret argocd-initial-admin-secret -o jsonpath=\"{.data.password}\" -n argo-cd | base64 -d'"
-echo "Go to http://localhost:8080"
